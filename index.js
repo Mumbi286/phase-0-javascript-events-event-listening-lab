b233da8ef6222e7c8952fd7f9f991ca341a2f39c
Christine Mumbi <christinemumbi286@gmail.com>
2025-09-14 22:18:40 +0300

    Adds index.js file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    let input = document.getElementById('button')
+    input.addEventListener('click', function(){
+        alert('binds an event listener')
+    })
+    }
+input.addEventListener('click', clickAlert)
+
+
